63876cbdff56aa390402bb371c6820307d26c0b3
Santiago Orozco <san7yz@gmail.com>
2023-02-28 02:49:09 +0000

    Final

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -19,7 +19,7 @@ const InputMessage = ({ input, setInput, sendMessage }: any) => (
       type="text"
       aria-label="chat input"
       required
-      className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-textinput px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm"
+      className="min-w-0 text-zinc-300 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-textinput px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-100 focus:border-orange-500 focus:outline-none focus:ring-4 focus:ring-orange-500/10 sm:text-sm"
       value={input}
       onKeyDown={(e) => {
         if (e.key === "Enter") {
@@ -99,7 +99,7 @@ export function Chat() {
       {loading && <LoadingChatLine />}
 
       {messages.length < 2 && (
-        <span className="mx-auto flex flex-grow text-gray-600 clear-both">
+        <span className="mx-auto flex flex-grow text-zinc-500 clear-both">
           Type a message to start the conversation
         </span>
       )}
