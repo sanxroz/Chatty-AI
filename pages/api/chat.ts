85c0b83039147cf9ae810425b68464cb56ffe1da
Santiago Orozco <san7yz@gmail.com>
2023-03-06 23:51:11 +0000

    Changes

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -7,7 +7,7 @@ if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing Environment Variable OPENAI_API_KEY");
 }
 
-const botName = "Chatty";
+const botName = "AI";
 const userName = "News reporter"; // TODO: move to ENV var
 const firstMessge = initialMessages[0].message;
 
@@ -52,8 +52,17 @@ export default async function handler(req: NextRequest) {
     : defaultPrompt;
 
   const payload = {
-    model: "gpt-3.5-turbo",
-    messages: [`user:`, `${finalPrompt}:`]
+    model: "text-davinci-003",
+    prompt: finalPrompt,
+    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
+    max_tokens: process.env.AI_MAX_TOKENS
+      ? parseInt(process.env.AI_MAX_TOKENS)
+      : 200,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop: [`${botName}:`, `${userName}:`],
+    user: body?.user,
   };
 
   const requestHeaders: Record<string, string> = {
@@ -64,7 +73,8 @@ export default async function handler(req: NextRequest) {
   if (process.env.OPENAI_API_ORG) {
     requestHeaders["OpenAI-Organization"] = process.env.OPENAI_API_ORG;
   }
- const response = await fetch("https://api.openai.com/v1/chat/completions", {
+
+  const response = await fetch("https://api.openai.com/v1/completions", {
     headers: requestHeaders,
     method: "POST",
     body: JSON.stringify(payload),
@@ -78,6 +88,7 @@ export default async function handler(req: NextRequest) {
       text: `ERROR with API integration. ${data.error.message}`,
     });
   }
+
   // return response with 200 and stringify json text
   return NextResponse.json({ text: data.choices[0].text });
 }
