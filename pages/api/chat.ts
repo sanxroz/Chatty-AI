abfb2a411d144a045a160cc6e6a8901b348813fd
Santiago Orozco <san7yz@gmail.com>
2023-03-06 23:44:50 +0000

    Update chat.ts

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -53,16 +53,7 @@ export default async function handler(req: NextRequest) {
 
   const payload = {
     model: "gpt-3.5-turbo",
-    prompt: finalPrompt,
-    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
-    max_tokens: process.env.AI_MAX_TOKENS
-      ? parseInt(process.env.AI_MAX_TOKENS)
-      : 200,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stop: [`${botName}:`, `${userName}:`],
-    user: body?.user,
+    messages: [`user:`, `${finalPrompt}:`]
   };
 
   const requestHeaders: Record<string, string> = {
