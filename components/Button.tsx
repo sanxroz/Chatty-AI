061ad1e8f579c43656431d31c65b73364aecdb2d
Santiago Orozco <san7yz@gmail.com>
2023-03-07 01:32:36 +0000

    Update Button.tsx and Chat.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ export function Button({ className, ...props }: any) {
   return (
     <button
       className={clsx(
-        "inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none",
+        "inline-flex items-center gap-2 justify-center rounded-full py-2 px-4 text-sm outline-offset-2 transition active:transition-none",
         "bg-orange-700 font-semibold text-zinc-100 shadow-md",
         className
       )}
